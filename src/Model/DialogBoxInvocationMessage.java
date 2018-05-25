af95cf56436f15050f7c6a13221a33ccfcba8cfd
Robbe Keters <robbeketers@gmail.com>
2018-05-25 15:05:12 +0200

    part 11

--- src/Model/DialogBoxInvocationMessage.java
+++ src/Model/DialogBoxInvocationMessage.java
@@ -55,8 +55,8 @@ public class DialogBoxInvocationMessage extends DialogBox{
 		Button add = new Button("+", xI+75, yI - 50 + listBox.getHeight(), Button.Type.TEXT);
 
 		Button remove = new Button("X", xI+75, yI - 100, Button.Type.TEXT);
-		Button moveUp = new Button("/\\", xI+75, yI- 100 + i.getLabel().getHeight(), Button.Type.TEXT);
-		Button moveDown = new Button("\\/",xI+75, yI - 100 + i.getLabel().getHeight()*2, Button.Type.TEXT);
+		Button moveUp = new Button("/\\", xI+75, yI- 100 + 30, Button.Type.TEXT);
+		Button moveDown = new Button("\\/",xI+75, yI - 100 + 30*2, Button.Type.TEXT);
 		
 		if(!listBox.getSelected()){
 			remove.setDisabled(false);
@@ -71,10 +71,11 @@ public class DialogBoxInvocationMessage extends DialogBox{
 		// add is geselecteerd bij instantiatie 
 		add.setSelectedControl(true);
 		
+		super.addButton(add);
 		super.addButton(moveDown);
 		super.addButton(moveUp);
 		super.addButton(remove);
-		super.addButton(add);
+		
 		
 //		
 		// Add all buttons en textboxes to one list 
--- src/View/representation/ListBoxRepresentation.java
+++ src/View/representation/ListBoxRepresentation.java
@@ -13,13 +13,14 @@ public class ListBoxRepresentation {
 	private String text;
 	private ArrayList<Label> arguments = new ArrayList<Label>();
 	private int x, y, width, height;
+	private boolean selected;
 	
 	public ListBoxRepresentation(ListBox lb) {
 //		super(x, y, width, height);
 		this.width = lb.getWidth();
 		this.height = lb.getHeight();
-		this.x = lb.getOriginX() + width/2;
-		this.y = lb.getOriginY() + height/2;
+		this.x = lb.getOriginX();
+		this.y = lb.getOriginY();
 //		this.text = text;
 //		for(int i = 0; i < 10; i++){
 //			Label label = new Label("");
@@ -32,7 +33,11 @@ public class ListBoxRepresentation {
 	public void draw(Canvas c, Graphics g){
 //		super.draw(c, g);
 //		drawText(c, g);
-//		drawRectangle(c, g);
+//		if(selected)
+			g.setColor(Color.BLUE);
+//		else
+//			g.setColor(Color.BLACK);
+		drawRectangle(c, g);
 //		for(int i = 0; i < arguments.size(); i++){
 ////			if((g.getFontMetrics().getHeight() + 2)*(i+1) <= height){
 //				if(arguments.get(i).getSelected())
@@ -48,14 +53,15 @@ public class ListBoxRepresentation {
 //		g.drawRect(x + c.getOrigineX(), y + c.getOrigineY(), width, height);
 //	}
 	
-	private void drawText(Canvas c, Graphics g){
-//		int stringWidth = g.getFontMetrics().stringWidth(text);
-//		int stringHeight = g.getFontMetrics().getHeight();
-//		int margin = 2;
-//		g.drawString(text, x + c.getOrigineX(), y + c.getOrigineY() - stringHeight/2);
+	private void drawRectangle(Canvas c, Graphics g){
+		g.drawRect(x, y, width, height);
 	}
 	
 	public void setArguments(ArrayList<Label> args){
 		this.arguments = args;
+	}
+	
+	public void setSelected(boolean s){
+		this.selected = s;
 	}
 }
